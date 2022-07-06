95db2b9b6e2aef0b6ea95c1740e2f8738a590d8e
Abdurrahman Rahim <imanabdurrahmanr@gmail.com>
2022-07-07 01:12:33 +0800

    Updated

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -5,12 +5,12 @@ export default function Countdown () {
         <div className='text-red-700 bg-white w-full h-1/2 '>
             <h1 className='text-center'>Countdown</h1>
             
-            <div className='items-center text-center mx-72 py-6'>
+            <div className='items-center text-center md:mx-72 sm:mx-0  py-6'>
                 <ul className='grid grid-cols-4'>
-                    <li className='rounded-lg shadow-xl h-48 '>Day</li>
-                    <li className='rounded-lg shadow-xl h-48 '>Hour</li>
-                    <li className='rounded-lg shadow-xl h-48 '>Minute</li>
-                    <li className='rounded-lg shadow-xl h-48 '>Second</li>
+                    <li className='rounded-lg shadow-xl h-36 md:h-48'>Day</li>
+                    <li className='rounded-lg shadow-xl h-36 md:h-48'>Hour</li>
+                    <li className='rounded-lg shadow-xl h-36 md:h-48'>Minute</li>
+                    <li className='rounded-lg shadow-xl h-36 md:h-48'>Second</li>
                 </ul>
             </div>
             
